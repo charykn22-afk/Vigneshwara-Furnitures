ea7dc453cde15e14fbdd10dac23f03772dcbb601
KANUKUNTLA RAJKOTI CHARY <chary.kn22@gmail.com>
2026-02-15 12:33:58 +0530

    added base name in the vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Vigneshwara-Furnitures/', // Ensures correct asset paths when deployed to GitHub Pages
 })
